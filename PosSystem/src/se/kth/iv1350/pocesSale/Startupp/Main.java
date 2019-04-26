faeef7365364428a1e9dbc709636f43e0f10ed60
JoakimHolmsten <joakim.schoolkth@gmail.com>
2019-04-26 21:25:48 +0200

    initigering
    Start av projet

---
+++ PosSystem/src/se/kth/iv1350/pocesSale/Startupp/Main.java
@@ -0,0 +1,21 @@
+package se.kth.iv1350.pocesSale.Startupp;
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.Util;
+
+import java.lang.*;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hej");
+        Scanner s = new Scanner(System.in);
+
+        String t = s.next();
+        System.out.println(t);
+
+        s.close();
+
+
+
+    }
+}
